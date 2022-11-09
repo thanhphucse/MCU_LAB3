84988ce2e20a731b42554ddd632c66eb637371e4
thanhphu <phu.nguyen03022002@hcmut.edu.vn>
2022-11-09 16:31:41 +0700

    push to github

---
+++ Core/Inc/control_7SEG.h
@@ -0,0 +1,16 @@
+/*
+ * control_7SEG.h
+ *
+ *  Created on: Nov 7, 2022
+ *      Author: Welcome
+ */
+
+#ifndef INC_CONTROL_7SEG_H_
+#define INC_CONTROL_7SEG_H_
+
+void display7SEG_horizontal(int num);
+void display7SEG_vertical(int num);
+void update7SEG_horizontal(int index);
+void update7SEG_vertical(int index);
+
+#endif /* INC_CONTROL_7SEG_H_ */
